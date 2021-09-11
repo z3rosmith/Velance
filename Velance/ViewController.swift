06027738598bd822a4614f8c74d1dd2595df30cb
김영채 <kevinkim2586@gmail.com>
2021-09-11 12:52:26 +0900

    initial commit

---
+++ Velance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Velance
+//
+//  Created by Kevin Kim on 2021/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
