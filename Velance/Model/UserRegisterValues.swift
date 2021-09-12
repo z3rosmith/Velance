216f6f0d02f564d5f7c3372a202705941d7e2bc1
김영채 <kevinkim2586@gmail.com>
2021-09-12 22:55:47 +0900

    feat: add UserRegisterValues

---
+++ Velance/Model/UserRegisterValues.swift
@@ -0,0 +1,25 @@
+//
+//  UserRegisterValues.swift
+//  Velance
+//
+//  Created by Kevin Kim on 2021/09/12.
+//
+
+import Foundation
+
+struct UserRegisterValues {
+    
+    private init() {}
+    
+    static let shared = UserRegisterValues()
+    
+    var userID: String = ""
+    var userPassword: String = ""
+    
+    var veganType: Int = 4
+    var height: Int = 150
+    var weight: Int = 45
+    var gender: Int = 0
+    var reason: Int = 0
+    
+}
