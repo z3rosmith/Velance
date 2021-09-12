2845065b947e2a80e18e025650de1c5b4e77f20f
김영채 <kevinkim2586@gmail.com>
2021-09-12 21:45:47 +0900

    feat: implement ChooseTypeVC UI

--- Velance/Constants.swift
+++ Velance/Constants.swift
@@ -11,17 +11,29 @@ struct Color {
     
     static let appDefaultColor              = "AppDefaultColor"
     static let appBackgroundColor           = "AppBackgroundColor"
+    static let appTintColor                 = "AppTintColor"
     
     //Tab Bar Colors
     static let tabBarSelectedColor          = "TabBarSelectedColor"
     static let tabBarUnselectedColor        = "TabBarUnselectedColor"
+    
+    
 
 }
 
 struct Images {
     
+    // Tab Bar Icons
     static let homeTabBarIcon_selected         = "home_selected"
     static let homeTabBarIcon_unselected       = "home_unselected"
     static let shopTabBarIcon_selected         = "shopping_selected"
     static let shopTabBarIcon_unselected       = "shopping_unselected"
+    
+    // Vegan Types
+    static let vegan            = "vegan"
+    static let ovo              = "ovo"
+    static let lacto            = "lacto"
+    static let lacto_ovo        = "lactoovo"
+    static let pesco            = "pesco"
+    
 }
--- Velance/UserRegister/ChooseTypeViewController.swift
+++ Velance/UserRegister/ChooseTypeViewController.swift
@@ -9,21 +9,56 @@ import UIKit
 
 class ChooseTypeViewController: UIViewController {
 
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet var buttonViews: [UIView]!
+    
+    private var selectedIndex: Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        configure()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func pressedNextButton(_ sender: UIButton) {
+        
+        
     }
-    */
+    
+    @IBAction func pressedVeganType(_ sender: UIButton) {
+        buttonViews[selectedIndex].backgroundColor = UIColor(named: Color.appTintColor)
+        selectedIndex = sender.tag
+        buttonViews[selectedIndex].backgroundColor = #colorLiteral(red: 0.5058823529, green: 0.7254901961, blue: 0.2235294118, alpha: 1)
+        
+        
+    }
+    
+    
+    
 
 }
+
+//MARK: - UI Configuration
+
+extension ChooseTypeViewController {
+    
+    private func configure() {
+        configureBottomView()
+        configureButtonViews()
+    }
+    
+    private func configureBottomView() {
+        bottomView.layer.cornerRadius = 30
+        bottomView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+    }
+    
+    private func configureButtonViews() {
+        buttonViews.forEach { view in
+            view.layer.cornerRadius = view.frame.height / 2
+            view.layer.borderWidth = 0.3
+            view.layer.borderColor = #colorLiteral(red: 0.5058823529, green: 0.7254901961, blue: 0.2235294118, alpha: 1)
+        }
+    }
+    
+}
+
