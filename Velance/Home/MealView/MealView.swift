fe4753708615165ad1a30626dbeb208ac34d6887
Jinyoung Kim <52317025+z3rosmith@users.noreply.github.com>
2021-09-27 13:04:23 +0900

    chore: Add files and set basic settings

---
+++ Velance/Home/MealView/MealView.swift
@@ -0,0 +1,20 @@
+//
+//  MealView.swift
+//  Velance
+//
+//  Created by Jinyoung Kim on 2021/09/16.
+//
+
+import UIKit
+
+class MealView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
